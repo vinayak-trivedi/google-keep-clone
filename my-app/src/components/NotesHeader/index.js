19c15197e9e50c3a78ad636409e5bd44fddaa96e
vinayak <trivedivinayak57@gmail.com>
2023-12-05 18:11:59 +0530

    Notes header component

---
+++ my-app/src/components/NotesHeader/index.js
@@ -0,0 +1,10 @@
+import styles from './notesHeader.module.css';
+
+export default function NotesHeader() {
+  return (
+    <header className={styles.header}>
+      <img src="/keep_icon.svg" width='60px' height='60px' />
+      <h1>Google Keep Clone</h1>
+    </header>
+  );
+}
