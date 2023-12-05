2272405e7496342da0a22032cd50eae8c9f7f14b
vinayak <trivedivinayak57@gmail.com>
2023-12-05 20:59:37 +0530

    icon position change

--- my-app/src/components/Notes/NotesCard.js
+++ my-app/src/components/Notes/NotesCard.js
@@ -3,14 +3,18 @@ import styles from './notesCard.module.css';
 export default function NotesCard({ title, note, deleteNote }) {
   return (
     <div className={styles.notes_card}>
-      <button onClick={deleteNote} className={styles.delete_button}>
-        <img src="/delete_icon.svg" width="100%" height="100%" />
-      </button>
       <div className={styles.text_container}>
         <p className={styles.notes_card__title}>{title}</p>
         <p className={styles.notes_card__note}>{note}</p>
       </div>
-      <div></div>
+      <div className={styles.note_actions}>
+        <button className={styles.color_select_button}>
+          <img src="/color_palette.png" width="100%" height="100%" />
+        </button>
+        <button onClick={deleteNote} className={styles.delete_button}>
+          <img src="/delete_icon.svg" width="100%" height="100%" />
+        </button>
+      </div>
     </div>
   );
 }
